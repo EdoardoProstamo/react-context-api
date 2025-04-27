e46354c8c5b0335e307640a2661555d4fc68a1f7
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-04-27 15:59:24 +0200

    feat: impostazione App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 
 function App() {
 
+  // useState
+  const [postList, setPostList] = useState([]);
+
+  // axios
+  const getPost = () => {
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+      .then(res => setPostList(res.data))
+  };
+
+  // useEffect
+  useEffect(() => { getPost() }, []);
+
+
+
+
   return (
     <>
+
     </>
   )
 }
