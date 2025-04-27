883f417af4e3d588c76f0c2cbebd9be120611102
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-04-27 20:00:06 +0200

    feat: PostList

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,7 +1,12 @@
+import PostCard from "./PostCard";
+
 function PostList() {
     return (
-        <></>
+        <>
+            {postList.map((post) => <PostCard post={post} key={post.id} />)}
+        </>
     )
+
 };
 
 export default PostList;
